9e9c4f593182e87c372b1c8caf42b8c538222298
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-16 22:44:48 -0500

    refactor: remove unnecessary ec2 instance

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "vm_apache_airflow_ip" {
-  description = "Public IP of Apache Airflow VM"
-  value       = aws_instance.apache_airflow.public_ip
-}
-
 output "incident_notifications_topic_arn" {
   value = aws_sns_topic.incident_notifications.arn
 }
